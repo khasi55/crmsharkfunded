94aa4015fce75aa25acd6e4f6a93dc0b29f3d17b
VISWANATH REDDY <khasireddy3@gmail.com>
2026-01-26 19:35:41 +0530

    update dashboard title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "SharkFunded CRM",
+    title: "Dashboard | SharkFunded ",
     description: "SharkFunded Trading Platform",
 };
 
